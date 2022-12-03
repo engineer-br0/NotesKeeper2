925ce43f6d95fb86d84f813066ecdea4b4256070
engineer-br0 <106507655+engineer-br0@users.noreply.github.com>
2022-12-03 16:00:57 +0530

    ui + add note

---
+++ script.js
@@ -0,0 +1,43 @@
+window.onload = function(e){ 
+    var data = [];
+    
+var title = document.getElementById("title");
+var add = document.getElementById("add");
+var tag = document.getElementById("tag");
+var content = document.getElementById("content");
+var notes = document.getElementById("notes");
+showData()
+
+function showData(){
+
+if(data){
+    var tbData = ""
+
+
+    for(let i = 0; i<data.length; i++){
+        tbData += `<div class="note">
+                      <img src="/del.png" onclick="deleteFunc(i)"/>
+                      <h2>${data[i].title}</h2>
+                      <h3>${data[i].tag}</h3>
+                      <p>${data[i].content}</p>
+                    </div>`
+    }
+    notes.innerHTML = tbData
+}
+}
+
+add.onclick = (e) => {
+    data.push(
+        {title: title.value,
+         tag : tag.value,
+         content:content.value
+         });
+    showData()
+}
+
+function deleteFunc(i) {
+    console.log(i)
+}
+
+
+};
